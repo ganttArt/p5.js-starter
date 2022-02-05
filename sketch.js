2f0f668f72a8b958679994c8d0713e54f85b4065
Chris Gantt <gantt.art@gmail.com>
2022-02-05 10:57:21 -1000

    Add starter code for any p5.js project

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+// //   sample code
+//   if (mouseIsPressed) {
+//     fill(255);
+//   } else {
+//     fill(0);
+//   }
+//  ellipse(mouseX, mouseY, 80, 80)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
